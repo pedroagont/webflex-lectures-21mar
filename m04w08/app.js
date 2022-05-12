b851f36bb975db233b9b3c384577f4e05c47e85c
Pedro A. González <pgonzalez@post.com>
2022-05-12 13:21:49 -0500

    Add Client Side JS, second part

--- m04w08/app.js
+++ m04w08/app.js
@@ -1,1 +1,15 @@
 console.log('Hi from the console! 👋💻👾');
+
+console.log(document);
+
+const header = document.getElementById('header');
+const headerTitle = document.querySelector('.header-title');
+const headerEmojis = document.querySelector('.header-emojis');
+
+// change content
+headerTitle.innerHTML = 'Hi from the console!';
+headerEmojis.innerHTML = '👾🤖👽';
+
+// change styles
+header.style.background = 'yellow';
+header.style.color = 'black';
