0983f21e52f02e3aebf3a884568e5564c3d491fe
Pedro A. González <pgonzalez@post.com>
2022-07-07 13:42:11 -0500

    Add React Fundamentals lecture code + readme + video link

---
+++ m07w16/my-app/src/components/App.js
@@ -0,0 +1,14 @@
+import Card from './Card';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello from React! 👋⚛️</h1>
+      <Card title="Card 1" description="This is my first card ever!" color="royalblue"/>
+      <Card title="Card 2" description="This is my second card!" color="tomato" />
+    </>
+  );
+}
+
+export default App;
---
+++ m07w16/my-app/src/components/Card.js
@@ -0,0 +1,12 @@
+function Card(props) {
+  const { title, description, color } = props;
+  
+  return (
+    <div className="card" style={ { backgroundColor: color } }>
+      <h3>{ title }</h3>
+      <p>{ description }</p>
+    </div>
+  );
+}
+
+export default Card;
